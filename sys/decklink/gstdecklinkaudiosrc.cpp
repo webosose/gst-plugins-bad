805e39fdf0b8f1c7cef434214abdbd12a2f6d72b
Andre McCurdy <armccurdy@gmail.com>
2016-02-09 14:00:00 -0800

    ensure valid sentinals for gst_structure_get() etc
    For GStreamer functions declared with G_GNUC_NULL_TERMINATED,
    ie __attribute__((__sentinel__)), gcc will generate a warning if the
    last parameter passed to the function is not NULL (where a valid NULL
    in this context is defined as zero with any pointer type).
    
    The C callers to such functions within gst-plugins-bad use the C NULL
    definition (ie ((void*)0)), which is a valid sentinel.
    
    However the C++ NULL definition (ie 0L), is not a valid sentinel
    without an explicit cast to a pointer type.
    
    Upstream-Status: Pending
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- sys/decklink/gstdecklinkaudiosrc.cpp
+++ sys/decklink/gstdecklinkaudiosrc.cpp
@@ -369,7 +369,7 @@ gst_decklink_audio_src_set_caps (GstBaseSrc * bsrc, GstCaps * caps)
       g_mutex_unlock (&self->input->lock);
 
       if (videosrc) {
-        g_object_get (videosrc, "connection", &vconn, NULL);
+        g_object_get (videosrc, "connection", &vconn, (void *) NULL);
         gst_object_unref (videosrc);
 
         switch (vconn) {
